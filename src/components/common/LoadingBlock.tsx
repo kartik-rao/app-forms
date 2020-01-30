60c08d8e2340e65b6a9d7858593c6b535efcd878
Kartik Rao <kartik.rao@globalred.com.au>
2020-01-30 13:35:09 +1100

    Refactor extract loading skeleton into component

---
+++ src/components/common/LoadingBlock.tsx
@@ -0,0 +1,19 @@
+import { Skeleton } from "antd";
+import { useLocalStore, useObserver } from "mobx-react-lite";
+import * as React from "react";
+import { appStoreContext } from "../../stores/AppStoreProvider";
+
+export interface IErrorBlockProps {
+    loading: boolean;
+}
+
+export const LoadingBlock : React.FC<IErrorBlockProps> = (props: IErrorBlockProps) => {
+    const store = React.useContext(appStoreContext);
+    if(!store) throw new Error("Store is null");
+
+    return useObserver(() => {
+        return <div className="fl-loading-display">
+            {props.loading && <Skeleton active />}
+        </div>
+    })
+}
